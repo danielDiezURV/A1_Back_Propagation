d13fb07cb4c0affc59900e2d45002b917c33040f
danid <danidiezsanchez@gmail.com>
2023-11-18 18:32:14 +0100

    +Fix matrix sizes +Added back_propagation() +Added update_weights() +Added calculate_error() used for loss_epoch function +Split data for test and validation in fit function +Added predict() Implemented complete test for A1_turbine using my NN -Pending learning... (loss value stuck at first epoch)

--- DataPreprocessing.py
+++ DataPreprocessing.py
@@ -4,21 +4,36 @@ import pandas as pd
 
 class DataPreprocessing:
 
-    def scale(self, dataset, s_min, s_max):
-        for col_idx in range(dataset.shape[1]):
-            col_values = dataset.iloc[:, col_idx]
-            x_min = min(col_values)
-            x_max = max(col_values)
-            dataset.iloc[:, col_idx] = col_values.apply(lambda x:  s_min + (((s_max - s_min)/(x_max - x_min))*(x - x_min)))
-        return dataset
+    def __init__(self):
+        self.x_min = 0
+        self.x_max = 1
+        self.s_min = 0
+        self.s_max = 1
 
-    def standardize(self, dataset):
+    def scale(self, dataset, s_min=0, s_max=1):
+        scaled_data = dataset.copy()
         for col_idx in range(dataset.shape[1]):
-            col_values = dataset.iloc[:, col_idx]
+            col_values = scaled_data[:, col_idx]
+            self.x_min = np.min(col_values)
+            self.x_max = np.max(col_values)
+            self.s_min = s_min
+            self.s_max = s_max
+            scaled_data[:, col_idx] = [self.s_min + (((self.s_max - self.s_min)/(self.x_max - self.x_min))*(x - self.x_min)) for x in col_values]
+        return scaled_data
+    
+    def inverse_scale(self, dataset):
+        scaled_data = dataset.copy()
+        for col_idx in range(dataset.shape[1]):
+            col_values = scaled_data[:, col_idx]
+            scaled_data[:, col_idx] = [((x - self.x_min) / (self.x_max - self.x_min)) * (self.s_max - self.s_min) + self.s_min for x in col_values]
+        return scaled_data
+    
+    def standardize(self, dataset):
+        scaled_data = dataset.copy()
+        for col_idx in range(dataset.shape[1]):
+            col_values = scaled_data.iloc[:, col_idx]
             mean = np.mean(col_values)
             stdev = np.std(col_values)
-            dataset.iloc[:, col_idx] = col_values.apply(lambda x: (x - mean) / stdev)
-        return dataset
-    
-
+            scaled_data.iloc[:, col_idx] = col_values.apply(lambda x: (x - mean) / stdev)
+        return scaled_data.values
 
--- MyNeuralNetwork.py
+++ MyNeuralNetwork.py
@@ -1,12 +1,14 @@
 from enum import Enum
 import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import *
 
 # Activation functions
 class ActivationFunction(Enum):
-  RELU = "RELU",lambda x: np.maximum(0, x)
-  TANH = "TANH",lambda x: np.tanh(x)
-  SIGMOID = "SIGMOID",lambda x: 1 / (1 + np.exp(-x))
-  LINEAR = "LINEAR",lambda x: x
+  RELU = "RELU",lambda x: np.maximum(0, x), lambda x: 1 if x >= 0 else 0
+  TANH = "TANH",lambda x: np.tanh(x), lambda x: 1 - np.tanh(x) ** 2
+  SIGMOID = "SIGMOID",lambda x: 1 / (1 + np.exp(-x)), lambda x: np.exp(-(1 / (1 + np.exp(-x)))) / (1 + np.exp(-(1 / (1 + np.exp(-x)))))
+  LINEAR = "LINEAR",lambda x: x, lambda x: 1
 
   @classmethod
   def get_function(cls, name):
@@ -15,26 +17,36 @@ class ActivationFunction(Enum):
         return function.value[1]
     raise ValueError(f"{name} is not a valid activation function")
   
+  @classmethod
+  def get_derivative(cls, name):
+    for function in cls:
+      if function.value[0] == name:
+        return function.value[2]
+    raise ValueError(f"{name} is not a valid activation function")
+  
 # Neural Network class
 class MyNeuralNetwork:
   def __init__(self, layers, nEpochs, learning_rate, momentum, activation, validation_set):
     # Neural network architecture
     self.L = len(layers)                                            # number of layers
-    self.n = layers.copy()                                          # number of neurons in each layer
+    self.n = layers                                                 # number of neurons in each layer
     self.h = []                                                     # units field
     self.xi = []                                                    # units activation
     self.w = []                                                     # edge weights
     self.theta = []                                                 # thresholds
     self.delta = []                                                 # propagation error
-    self.d_w = []                                                   # weights delta
-    self.d_theta = []                                               # thresholds delta
-    self.d_w_prev = []                                              # previous weights delta
+    self.w_prev = []                                                # previous weights delta
+    self.theta_prev = []                                            # previous thresholds delta
     # Learning params
     self.nEpochs = nEpochs                                          # number of epochs
     self.learning_rate = learning_rate                              # learning rate
     self.momentum = momentum                                        # momentum
     self.activation = ActivationFunction.get_function(activation.upper())  # activation function (sigmoid, relu, linear, tanh)
+    self.derivative = ActivationFunction.get_derivative(activation.upper()) # derivative of activation function
     self.validation_set = validation_set                            # Percentage of data used for validation
+    # Performance metrics
+    self.train_loss = np.zeros(nEpochs)          # train loss
+    self.val_loss = np.zeros(nEpochs)                # validation loss
 
     # Initialize Network
     self.xi.append(np.zeros(layers[0]))                             # input layer
@@ -43,50 +55,92 @@ class MyNeuralNetwork:
       self.xi.append(np.zeros(layers[l]))
       self.theta.append(np.random.rand(layers[l])) 
       self.delta.append(np.zeros(layers[l]))
-      self.d_theta.append(np.zeros(layers[l]))
+
 
     # Initialize weights
     self.w.append(np.random.rand(layers[0], layers[0])) 
-    self.d_w.append(np.zeros((layers[0], layers[0])))
-    self.d_w_prev.append(np.zeros((layers[0], layers[0])))
     for l in range(1, self.L):
       self.w.append(np.random.rand(layers[l], layers[l - 1])) 
-      self.d_w.append(np.zeros((layers[l], layers[l - 1])))
-      self.d_w_prev.append(np.zeros((layers[l], layers[l - 1])))
 
-    self.d_theta_prev = self.d_theta
+    self.w_prev = self.w.copy()
+    self.theta_prev = self.theta.copy()
 
   def fit(self, x, y):
-    loss = []
+    x_train, x_validate, y_train, y_validate = train_test_split(x, y, test_size=self.validation_set, shuffle=True)
     for epoch in range(self.nEpochs):
-      for i in np.random.permutation(len(x)):                      # for each random pattern xµ in the training set
-        x_prediction = self.feedForward(x.values[i])                     # feed-forward the network with xµ
-        
-        # TODO: ↓↓↓↓↓
-        # evaluate the network output yµ
-        # Back-propagate the error for this pattern
-        # Update the weights and thresholds        
-      # Feed-forward all training patterns and calculate their prediction quadratic error
-      # Feed-forward all validation patterns and calculate their prediction quadratic error
-    # Optional: Plot the evolution of the training and validation errors  
-    # Feed-forward all test patterns
-    # Descale the predictions of test patterns, and evaluate them
+      for i in np.random.permutation(len(x_train)):                      # for each random pattern xµ in the training set
+        x_prediction = self.feedForward(x_train[i])               # feed-forward the network with xµ
+        self.back_propagation(x_prediction, y_train[i])       # back-propagate the error for this pattern
+        self.update_weights()                                      # update the weights and thresholds
 
+      # Feed-forward all training patterns and calculate their prediction quadratic error
+      
+      (self.train_loss[epoch], train_mae) = self.calculate_error(x_train, y_train)
+      (self.val_loss[epoch], val_mae) = self.calculate_error(x_validate, y_validate)
+
+      print("Epoch ",epoch+1,"/",self.nEpochs)
+      print("============= [loss: ", round(self.train_loss[epoch], 4), " - mae: ", round(train_mae, 4), " - val_loss: ", round(self.val_loss[epoch], 4), " - val_mae:  ", round(val_mae, 4),"]")
+
+  def predict(self, x):
+    predictions = np.zeros((len(x), 1))
+    for i in range(len(x)):
+      predictions[i][0] = self.feedForward(x[i])
+    return predictions
+
+  def loss_epochs(self):  
+    return self.train_loss, self.val_loss
+  
   def feedForward(self, x):
     # Copy input values to input layer
     self.xi[0] = x
-    # Feed-forward first layer
-    for i in range(self.n[0]):
-        self.h[0][i] = sum(self.w[0][i][j] * self.xi[0][j] for j in range(self.n[0])) - self.theta[0][i]
-        self.xi[1][i] = self.activation(self.h[0][i])
     # Feed-forward hidden layers
-    for l in range(1, self.L):
+    for l in range(self.L):
       for i in range(self.n[l]):
-        self.h[l][i] = sum(self.w[l][i][j] * self.xi[l][j] for j in range(self.n[l-1])) - self.theta[l][i]
+        self.h[l][i] = np.dot(self.w[l][i], self.xi[l]) - self.theta[l][i]
         self.xi[l+1][i] = self.activation(self.h[l][i])
-    # Return output layer
-    return self.xi[self.L]
-    
+    # Return o utput layer
+    return self.xi[self.L]  
 
 
-  
+  def back_propagation(self, x_predicted, y_expected):
+    # Compute output layer error
+    for i in range(self.n[self.L-1]):
+      self.delta[self.L-1][i] = self.derivative((self.h[self.L-1][i])) * (x_predicted - y_expected)
+    # Compute hidden layers error
+    for l in reversed(range(1,self.L)):
+      for j in range(self.n[l-1]):
+        sum_delta_x_weight = 0
+        for i in range(self.n[l]):   
+          sum_delta_x_weight += self.w[l][i][j] * self.delta[l][i]    
+        self.delta[l-1][j] = sum_delta_x_weight * self.derivative(self.h[l-1][j])
+
+
+  def update_weights(self):
+    for i in range(self.n[0]):
+      for j in range(self.n[0]):
+        self.w_prev[0][i][j] = (self.momentum * self.w_prev[0][i][j]) -(self.learning_rate * self.delta[0][i] * self.xi[1][i]) 
+        self.w[0][i][j] += self.w_prev[0][i][j]
+
+    self.theta_prev[0] = (self.momentum * self.theta_prev[0]) - (self.learning_rate * self.delta[0][i])
+    self.theta[0] += self.theta_prev[0]
+
+      
+    for l in range(1,self.L):
+      for i in range(self.n[l]):
+        for j in range(self.n[l-1]):
+          self.w_prev[l][i][j] = -(self.learning_rate * self.delta[l][i] * self.xi[l+1][i]) + (self.momentum * self.w_prev[l][i][j])
+          self.w[l][i][j] += self.w_prev[l][i][j]
+
+      self.theta_prev[l] = -(self.learning_rate * self.delta[l][i]) + (self.momentum * self.theta_prev[l])
+      self.theta[l] += self.theta_prev[l]
+
+  def calculate_error(self, x, y):
+      predictions = np.zeros(len(x))
+      for i in range(len(x)):
+        predictions[i] = self.feedForward(x[i])
+      return(mean_squared_error(predictions, y), mean_absolute_error(predictions, y))
+
+
+
+
+
